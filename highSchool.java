8b6a5bf7ee0e869f59573f4d655d6f6559809d08
AndrewBiggam <60410622+AndrewBiggam@users.noreply.github.com>
2020-02-05 14:58:31 -0600

    Add files via upload

---
+++ highSchool.java
@@ -0,0 +1,25 @@
+class highSchool{
+
+	public String runLength(String a) {
+	
+	String output;
+	int current;
+	int next;
+	
+	for (int i = 0; i < a.length(); i++){
+        if(a.charAt(i)!=a.charAt(i)+1){
+			
+		}
+	
+	
+	
+	output = 
+	
+	char c = a.charAt(i); 
+	
+	}
+	
+	public String runLength(String, String) {
+	
+	}
+}
