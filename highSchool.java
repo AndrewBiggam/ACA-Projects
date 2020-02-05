5f79a97f80e21688ed72a99316d9cd6c27367530
AndrewBiggam <60410622+AndrewBiggam@users.noreply.github.com>
2020-02-05 16:33:03 -0600

    Add files via upload

--- highSchool.java
+++ highSchool.java
@@ -7,20 +7,22 @@ class highSchool
 	{
 		for (int i = 0; i < a.length(); i++)
 		{
-        
-			if(a.charAt(i)==a.charAt(i)+1)
-			{
-					counter += counter;
-			}
-		
-		
-			if(a.charAt(i)!=a.charAt(i)+1)
-			{
-				String num = Integer.toString(counter);
-				char letter = a.charAt(i);
-				output += num +"" + letter;
-			}
-			counter = 1;
+			if(i + 1 == a.length())
+				break;
+			
+				if(a.charAt(i)==a.charAt(i+1))
+				{
+						counter += counter;
+				}
+			
+			
+				if(a.charAt(i)!=a.charAt(i+1))
+				{
+					String num = Integer.toString(counter);
+					char letter = a.charAt(i);
+					output += num +"" + letter;
+				}
+				counter = 1;
 		}
 		return output;
 	}
