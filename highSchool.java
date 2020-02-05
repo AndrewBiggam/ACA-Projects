ce3dacd485d8f790f22c52cceceaf8c799063c31
AndrewBiggam <60410622+AndrewBiggam@users.noreply.github.com>
2020-02-05 16:13:11 -0600

    Add files via upload

--- highSchool.java
+++ highSchool.java
@@ -1,28 +1,38 @@
-class highSchool{
-
-	public String runLength(String a) {
-	
+class highSchool
+{
 	String output;
 	int counter = 1;
 	
-	
-	for (int i = 0; i < a.length(); i++){
+	public String runLength(String a) 
+	{
+		for (int i = 0; i < a.length(); i++)
+		{
         
-		 if(a.charAt(i)==a.charAt(i)+1){
-				 counter += counter;
-		}
+			if(a.charAt(i)==a.charAt(i)+1)
+			{
+					counter += counter;
+			}
 		
 		
-		if(a.charAt(i)!=a.charAt(i)+1){
-			
-			String num = Integer.toString(counter);
-			(num + a.charAt(i))+=output;
+			if(a.charAt(i)!=a.charAt(i)+1)
+			{
+				String num = Integer.toString(counter);
+				char letter = a.charAt(i);
+				output += num +"" + letter;
+			}
 			counter = 1;
 		}
 		return output;
 	}
-	
-	public String runLength(String, String) {
-	
+	//public String runLength(String b, String c) {
+	//
+	//}
+	public static void main(String[] args)
+	{
+		highSchool tester = new highSchool();
+		String testOne = tester.runLength("AAAAABBBBB");
+		System.out.println(testOne);
+		String testTwo = tester.runLength("SSSSSSSS");
+		System.out.println(testTwo);
 	}
 }
