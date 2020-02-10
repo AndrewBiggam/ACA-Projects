8dda19de4e2f0eb5df5f78488018841ec1afe73e
AndrewBiggam <60410622+AndrewBiggam@users.noreply.github.com>
2020-02-10 16:57:28 -0600

    Add files via upload

--- dinero.java
+++ dinero.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
 import java.util.*;
 class robertD{
 	
@@ -5,12 +7,6 @@ class robertD{
 	
 	enum Moneys{
 		penny, nickel, dime, quarter, halfdollar, dollar;
-		public static Moneys randomcoin()
-		{
-			Random r = new Random();
-			int newcoin = r.nextInt(6);
-			return values()[newcoin];
-		}
 	}
 
 	public int getValue(Moneys coin){
@@ -41,26 +37,70 @@ class robertD{
 	}
 }
 
-class dinero{
+class dinero extends JFrame{
+	
+	JPanel coinWindow = new JPanel();
+	
+	JTextField coinText = new JTextField(38);
+	JTextField sumText = new JTextField("Default Text", 38);
+	
+	JTextArea txtArea = new JTextArea( 5, 37 );
+	
+	JScrollPane pane = new JScrollPane( txtArea );
+	
+	public dinero()
+	{
+		super("My Pocket");
+		setSize(500, 200);
+		setDefaultCloseOperation( EXIT_ON_CLOSE );
+		add(coinWindow);
+		
+		txtArea.setLineWrap(true);
+		txtArea.setWrapStyleWord(true) ;
+		
+		pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		coinWindow.add(coinText);
+		coinWindow.add(sumText);
+		coinWindow.add(pane);
+		
+		setVisible( true );
+	}
+	
+	public static robertD.Moneys randomcoin()
+	{
+		Random r = new Random();
+		int newcoin = r.nextInt(6);
+		return robertD.Moneys.values()[newcoin];
+	}
+	
+	
 	public static void main(String... v){
 		robertD Duckets = new robertD();
 		
 		Duckets.pocketChange = new robertD.Moneys[20];
 	
+		DecimalFormat twoPlaces = new DecimalFormat("0.00");
+		
+		dinero gui = new dinero();
+	
 		int sum = 0;
+		
 	
 		for(int i = 0; i < Duckets.pocketChange.length; i++)
 		{
-			Duckets.pocketChange[i] = robertD.Moneys.randomcoin();
+			Duckets.pocketChange[i] = randomcoin();
 		}
 		
-		// give each of the coins a value
-		for(robertD.Moneys coin: Duckets.pocketChange){ //might need different for loop
-			System.out.println(Duckets.getValue(coin) );
+	
+		for(robertD.Moneys coin: Duckets.pocketChange){ 
+			int value = Duckets.getValue(coin);
+			
+			System.out.println("$" + twoPlaces.format((double)value/100) + " " + coin);
 			sum += Duckets.getValue(coin);
 		}
 		System.out.println();
-		System.out.println("Dis is how much moolah you haz: " + "$" + (double)sum/100);
+		System.out.println("This is how poor you are: " + "$" + (double)sum/100);
 		
 		
 	}
